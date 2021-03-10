5179a6a1280194b9ac436cfd36d5dce1bcea9800
Luke Merrett <lukeamerrett@gmail.com>
2021-03-10 13:46:19 +0000

    First tutorial file

---
+++ src/a_introduction/01_HelloWorld.kt
@@ -0,0 +1,16 @@
+// Packages are optional, if not specified the contents go into the default package
+package a_introduction
+
+// Entry point for a Kotlin application
+// Semi colons are optional
+fun main() {
+    println("Hello, World!")
+}
+
+
+/*
+    // Can also be declared with args to capture command line arguments
+    fun main(args: Array<String>) {
+        println("Hello, World!")
+    }
+*/
